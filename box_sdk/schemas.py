e585690064de4de9424d4630cef13821cc7cb7bd
box-sdk-build <sdk-build@box.com>
2023-07-12 09:05:53 -0700

    generated with codegen at box/box-codegen@26006c5 and spec at box/box-openapi@d94aa0b

--- box_sdk/schemas.py
+++ box_sdk/schemas.py
@@ -5700,8 +5700,11 @@ class SearchResultWithSharedLink(BaseObject):
         self.item = item
         self.type = type
 
+class SearchResultsWithSharedLinksTypeField(str, Enum):
+    SEARCH_RESULTS_WITH_SHARED_LINKS = 'search_results_with_shared_links'
+
 class SearchResultsWithSharedLinks(BaseObject):
-    def __init__(self, total_count: Optional[int] = None, limit: Optional[int] = None, offset: Optional[int] = None, entries: Optional[List[SearchResultWithSharedLink]] = None, **kwargs):
+    def __init__(self, total_count: Optional[int] = None, limit: Optional[int] = None, offset: Optional[int] = None, type: Optional[SearchResultsWithSharedLinksTypeField] = None, entries: Optional[List[SearchResultWithSharedLink]] = None, **kwargs):
         """
         :param total_count: One greater than the offset of the last entry in the search results.
             The total number of entries in the collection may be less than
@@ -5714,6 +5717,8 @@ class SearchResultsWithSharedLinks(BaseObject):
         :param offset: The 0-based offset of the first entry in this set. This will be the same
             as the `offset` query parameter used.
         :type offset: Optional[int], optional
+        :param type: Specifies the response as search result items with shared links
+        :type type: Optional[SearchResultsWithSharedLinksTypeField], optional
         :param entries: The search results for the query provided, including the
             additional information about any shared links through
             which the item has been shared with the user.
@@ -5723,10 +5728,14 @@ class SearchResultsWithSharedLinks(BaseObject):
         self.total_count = total_count
         self.limit = limit
         self.offset = offset
+        self.type = type
         self.entries = entries
 
+class SearchResultsTypeField(str, Enum):
+    SEARCH_RESULTS_ITEMS = 'search_results_items'
+
 class SearchResults(BaseObject):
-    def __init__(self, total_count: Optional[int] = None, limit: Optional[int] = None, offset: Optional[int] = None, entries: Optional[List[Union[File, Folder, WebLink]]] = None, **kwargs):
+    def __init__(self, total_count: Optional[int] = None, limit: Optional[int] = None, offset: Optional[int] = None, type: Optional[SearchResultsTypeField] = None, entries: Optional[List[Union[File, Folder, WebLink]]] = None, **kwargs):
         """
         :param total_count: One greater than the offset of the last entry in the search results.
             The total number of entries in the collection may be less than
@@ -5739,6 +5748,8 @@ class SearchResults(BaseObject):
         :param offset: The 0-based offset of the first entry in this set. This will be the same
             as the `offset` query parameter used.
         :type offset: Optional[int], optional
+        :param type: Specifies the response as search result items without shared links
+        :type type: Optional[SearchResultsTypeField], optional
         :param entries: The search results for the query provided.
         :type entries: Optional[List[Union[File, Folder, WebLink]]], optional
         """
@@ -5746,6 +5757,7 @@ class SearchResults(BaseObject):
         self.total_count = total_count
         self.limit = limit
         self.offset = offset
+        self.type = type
         self.entries = entries
 
 class RecentItemInteractionTypeField(str, Enum):
