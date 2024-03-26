63070ae63fd67c76552dd7be51c061c7d05e9c7f
box-sdk-build <94016436+box-sdk-build@users.noreply.github.com>
2024-03-26 11:49:59 +0100

    test: Custom base URLs unit test (box/box-codegen#452) (#101)

--- test/client.py
+++ test/client.py
@@ -1,5 +1,7 @@
 from box_sdk_gen.internal.utils import to_string
 
+import pytest
+
 from box_sdk_gen.client import BoxClient
 
 from box_sdk_gen.schemas import UserFull
@@ -11,6 +13,8 @@ from test.commons import get_default_client
 from box_sdk_gen.networking.fetch import FetchOptions
 
 from box_sdk_gen.networking.fetch import FetchResponse
+
+from box_sdk_gen.networking.base_urls import BaseUrls
 
 client: BoxClient = get_default_client()
 
@@ -47,3 +51,14 @@ def testWithExtraHeaders():
     app_user: UserFull = as_user_client.users.get_user_me()
     assert to_string(app_user.name) == user_name
     client.users.delete_user_by_id(created_user.id)
+
+
+def testWithCustomBaseUrls():
+    new_base_urls: BaseUrls = BaseUrls(
+        base_url='https://box.com/',
+        upload_url='https://box.com/',
+        oauth_2_url='https://box.com/',
+    )
+    custom_base_client: BoxClient = client.with_custom_base_urls(new_base_urls)
+    with pytest.raises(Exception):
+        custom_base_client.users.get_user_me()
