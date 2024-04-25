b380ecb07da1479bc675be2671a81d5780231ef1
box-sdk-build <sdk-build@box.com>
2024-04-25 14:11:28 -0700

    feat(ts): support Date and DateTime manual types (box/box-codegen#476)

--- test/legal_hold_policies.py
+++ test/legal_hold_policies.py
@@ -2,6 +2,8 @@ from box_sdk_gen.schemas import LegalHoldPolicy
 
 from box_sdk_gen.schemas import LegalHoldPolicies
 
+from box_sdk_gen.internal.utils import DateTime
+
 from box_sdk_gen.internal.utils import get_uuid
 
 from box_sdk_gen.internal.utils import date_time_from_string
@@ -47,23 +49,23 @@ def testCreateUpdateGetDeleteLegalHoldPolicy():
 def testCreateNotOngoingLegalHoldPolicy():
     legal_hold_policy_name: str = get_uuid()
     legal_hold_description: str = 'test description'
+    filter_started_at: DateTime = date_time_from_string('2021-01-01T00:00:00-08:00')
+    filter_ended_at: DateTime = date_time_from_string('2022-01-01T00:00:00-08:00')
     legal_hold_policy: LegalHoldPolicy = (
         client.legal_hold_policies.create_legal_hold_policy(
             legal_hold_policy_name,
             description=legal_hold_description,
-            filter_started_at=date_time_from_string('2021-01-01T00:00:00-08:00'),
-            filter_ended_at=date_time_from_string('2022-01-01T00:00:00-08:00'),
+            filter_started_at=filter_started_at,
+            filter_ended_at=filter_ended_at,
             is_ongoing=False,
         )
     )
     assert legal_hold_policy.policy_name == legal_hold_policy_name
     assert legal_hold_policy.description == legal_hold_description
-    assert (
-        date_time_to_string(legal_hold_policy.filter_started_at)
-        == '2021-01-01T00:00:00-08:00'
-    )
-    assert (
-        date_time_to_string(legal_hold_policy.filter_ended_at)
-        == '2022-01-01T00:00:00-08:00'
-    )
+    assert date_time_to_string(
+        legal_hold_policy.filter_started_at
+    ) == date_time_to_string(filter_started_at)
+    assert date_time_to_string(
+        legal_hold_policy.filter_ended_at
+    ) == date_time_to_string(filter_ended_at)
     client.legal_hold_policies.delete_legal_hold_policy_by_id(legal_hold_policy.id)
