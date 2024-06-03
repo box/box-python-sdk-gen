f8d164d0242ee9ff98173d9c4e610e14b30d5bed
box-sdk-build <94016436+box-sdk-build@users.noreply.github.com>
2024-06-03 14:24:38 +0200

    test: Update sign request integration tests (box/box-codegen#499) (#180)

--- test/sign_requests.py
+++ test/sign_requests.py
@@ -12,6 +12,10 @@ from box_sdk_gen.schemas.file_base import FileBase
 
 from box_sdk_gen.schemas.sign_request_create_signer import SignRequestCreateSigner
 
+from box_sdk_gen.schemas.sign_request_create_signer import (
+    SignRequestCreateSignerRoleField,
+)
+
 from box_sdk_gen.schemas.folder_mini import FolderMini
 
 from box_sdk_gen.schemas.sign_request_prefill_tag import SignRequestPrefillTag
@@ -38,17 +42,60 @@ def testCreateGetCancelAndListSignRequest():
     file_to_sign: FileFull = upload_new_file()
     destination_folder: FolderFull = create_new_folder()
     created_sign_request: SignRequest = client.sign_requests.create_sign_request(
-        [SignRequestCreateSigner(email=signer_email)],
+        [
+            SignRequestCreateSigner(
+                email=signer_email,
+                suppress_notifications=True,
+                declined_redirect_url='https://www.box.com',
+                embed_url_external_user_id='123',
+                is_in_person=False,
+                login_required=False,
+                password='password',
+                role=SignRequestCreateSignerRoleField.SIGNER.value,
+            )
+        ],
         source_files=[FileBase(id=file_to_sign.id)],
         parent_folder=FolderMini(id=destination_folder.id),
+        is_document_preparation_needed=False,
+        redirect_url='https://www.box.com',
+        declined_redirect_url='https://www.box.com',
+        are_text_signatures_enabled=True,
+        email_subject='Sign this document',
+        email_message='Please sign this document',
+        are_reminders_enabled=True,
+        name='Sign Request',
         prefill_tags=[
             SignRequestPrefillTag(
                 date_value=date_from_string('2035-01-01'), document_tag_id='0'
             )
         ],
+        days_valid=30,
+        external_id='123',
+        is_phone_verification_required_to_view=False,
+        external_system_name='BoxSignIntegration',
     )
+    assert created_sign_request.are_reminders_enabled == True
+    assert created_sign_request.are_text_signatures_enabled == True
+    assert created_sign_request.days_valid == 30
+    assert created_sign_request.declined_redirect_url == 'https://www.box.com'
+    assert created_sign_request.email_message == 'Please sign this document'
+    assert created_sign_request.email_subject == 'Sign this document'
+    assert created_sign_request.external_id == '123'
+    assert created_sign_request.external_system_name == 'BoxSignIntegration'
+    assert created_sign_request.is_document_preparation_needed == False
+    assert created_sign_request.is_phone_verification_required_to_view == False
+    assert created_sign_request.name == 'Sign Request'
+    assert created_sign_request.redirect_url == 'https://www.box.com'
     assert created_sign_request.sign_files.files[0].name == file_to_sign.name
     assert created_sign_request.signers[1].email == signer_email
+    assert created_sign_request.signers[1].suppress_notifications == True
+    assert (
+        created_sign_request.signers[1].declined_redirect_url == 'https://www.box.com'
+    )
+    assert created_sign_request.signers[1].embed_url_external_user_id == '123'
+    assert created_sign_request.signers[1].is_in_person == False
+    assert created_sign_request.signers[1].login_required == False
+    assert to_string(created_sign_request.signers[1].role) == 'signer'
     assert created_sign_request.parent_folder.id == destination_folder.id
     assert (
         date_to_string(created_sign_request.prefill_tags[0].date_value) == '2035-01-01'
