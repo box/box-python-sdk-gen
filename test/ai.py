4ca27a268ecf5285955b09c80a408f59a754fffd
box-sdk-build <sdk-build@box.com>
2025-01-29 11:11:28 -0800

    test: Change tests for new response in `ai_extract_structured` (box/box-codegen#641)

--- test/ai.py
+++ test/ai.py
@@ -313,16 +313,22 @@ def testAIExtractStructuredWithFields():
         ],
         ai_agent=agent_ignoring_overriding_embeddings_model,
     )
-    assert to_string(get_value_from_object_raw_data(response, 'firstName')) == 'John'
-    assert to_string(get_value_from_object_raw_data(response, 'lastName')) == 'Doe'
+    assert to_string(
+        get_value_from_object_raw_data(response, 'answer.hobby')
+    ) == to_string(['guitar'])
     assert (
-        to_string(get_value_from_object_raw_data(response, 'dateOfBirth'))
+        to_string(get_value_from_object_raw_data(response, 'answer.firstName'))
+        == 'John'
+    )
+    assert (
+        to_string(get_value_from_object_raw_data(response, 'answer.lastName')) == 'Doe'
+    )
+    assert (
+        to_string(get_value_from_object_raw_data(response, 'answer.dateOfBirth'))
         == '1990-07-04'
     )
-    assert to_string(get_value_from_object_raw_data(response, 'age')) == '34'
-    assert to_string(get_value_from_object_raw_data(response, 'hobby')) == to_string(
-        ['guitar']
-    )
+    assert to_string(get_value_from_object_raw_data(response, 'answer.age')) == '34'
+    assert response.completion_reason == 'done'
     client.files.delete_file_by_id(file.id)
 
 
@@ -386,16 +392,22 @@ def testAIExtractStructuredWithMetadataTemplate():
             template_key=template_key, scope='enterprise'
         ),
     )
-    assert to_string(get_value_from_object_raw_data(response, 'firstName')) == 'John'
-    assert to_string(get_value_from_object_raw_data(response, 'lastName')) == 'Doe'
     assert (
-        to_string(get_value_from_object_raw_data(response, 'dateOfBirth'))
+        to_string(get_value_from_object_raw_data(response, 'answer.firstName'))
+        == 'John'
+    )
+    assert (
+        to_string(get_value_from_object_raw_data(response, 'answer.lastName')) == 'Doe'
+    )
+    assert (
+        to_string(get_value_from_object_raw_data(response, 'answer.dateOfBirth'))
         == '1990-07-04T00:00:00Z'
     )
-    assert to_string(get_value_from_object_raw_data(response, 'age')) == '34'
-    assert to_string(get_value_from_object_raw_data(response, 'hobby')) == to_string(
-        ['guitar']
-    )
+    assert to_string(get_value_from_object_raw_data(response, 'answer.age')) == '34'
+    assert to_string(
+        get_value_from_object_raw_data(response, 'answer.hobby')
+    ) == to_string(['guitar'])
+    assert response.completion_reason == 'done'
     client.metadata_templates.delete_metadata_template(
         DeleteMetadataTemplateScope.ENTERPRISE, template.template_key
     )
