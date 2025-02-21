0806a401dc19a5f50b2f7dfecc7fe0dd1447eb87
box-sdk-build <sdk-build@box.com>
2025-02-21 05:23:44 -0800

    docs: remove beta tag for AI extract endpoints (box/box-openapi#511)

--- box_sdk_gen/schemas/ai_ask.py
+++ box_sdk_gen/schemas/ai_ask.py
@@ -37,20 +37,16 @@ class AiAsk(BaseObject):
         **kwargs
     ):
         """
-                :param mode: The mode specifies if this request is for a single or multiple items. If you select `single_item_qa` the `items` array can have one element only. Selecting `multiple_item_qa` allows you to provide up to 25 items.
-                :type mode: AiAskModeField
-                :param prompt: The prompt provided by the client to be answered by the LLM. The prompt's length is limited to 10000 characters.
-                :type prompt: str
-                :param items: The items to be processed by the LLM, often files.
-
-        **Note**: Box AI handles documents with text representations up to 1MB in size, or a maximum of 25 files, whichever comes first.
-        If the file size exceeds 1MB, the first 1MB of text representation will be processed.
-        If you set `mode` parameter to `single_item_qa`, the `items` array can have one element only.
-                :type items: List[AiItemAsk]
-                :param dialogue_history: The history of prompts and answers previously passed to the LLM. This provides additional context to the LLM in generating the response., defaults to None
-                :type dialogue_history: Optional[List[AiDialogueHistory]], optional
-                :param include_citations: A flag to indicate whether citations should be returned., defaults to None
-                :type include_citations: Optional[bool], optional
+        :param mode: Box AI handles text documents with text representations up to 1MB in size, or a maximum of 25 files, whichever comes first. If the text file size exceeds 1MB, the first 1MB of text representation will be processed. Box AI handles image documents with a resolution of 1024 x 1024 pixels, with a maximum of 5 images or 5 pages for multi-page images. If the number of image or image pages exceeds 5, the first 5 images or pages will be processed. If you set mode parameter to `single_item_qa`, the items array can have one element only. Currently Box AI does not support multi-modal requests. If both images and text are sent Box AI will only process the text.
+        :type mode: AiAskModeField
+        :param prompt: The prompt provided by the client to be answered by the LLM. The prompt's length is limited to 10000 characters.
+        :type prompt: str
+        :param items: The items to be processed by the LLM, often files.
+        :type items: List[AiItemAsk]
+        :param dialogue_history: The history of prompts and answers previously passed to the LLM. This provides additional context to the LLM in generating the response., defaults to None
+        :type dialogue_history: Optional[List[AiDialogueHistory]], optional
+        :param include_citations: A flag to indicate whether citations should be returned., defaults to None
+        :type include_citations: Optional[bool], optional
         """
         super().__init__(**kwargs)
         self.mode = mode
