750876b5e2ce71fccaee51aa28008b2ab5cb0ceb
box-sdk-build <sdk-build@box.com>
2025-05-09 01:17:18 -0700

    feat: add support of IBM models to AI API (box/box-openapi#522)

--- box_sdk_gen/schemas/ai_agent_basic_gen_tool.py
+++ box_sdk_gen/schemas/ai_agent_basic_gen_tool.py
@@ -1,12 +1,6 @@
 from typing import Optional
 
-from typing import Union
-
-from box_sdk_gen.schemas.ai_llm_endpoint_params_open_ai import AiLlmEndpointParamsOpenAi
-
-from box_sdk_gen.schemas.ai_llm_endpoint_params_google import AiLlmEndpointParamsGoogle
-
-from box_sdk_gen.schemas.ai_llm_endpoint_params_aws import AiLlmEndpointParamsAws
+from box_sdk_gen.schemas.ai_llm_endpoint_params import AiLlmEndpointParams
 
 from box_sdk_gen.schemas.ai_agent_basic_text_tool_base import AiAgentBasicTextToolBase
 
@@ -35,13 +29,7 @@ class AiAgentBasicGenTool(AiAgentLongTextToolTextGen):
         prompt_template: Optional[str] = None,
         model: Optional[str] = None,
         num_tokens_for_completion: Optional[int] = None,
-        llm_endpoint_params: Optional[
-            Union[
-                AiLlmEndpointParamsOpenAi,
-                AiLlmEndpointParamsGoogle,
-                AiLlmEndpointParamsAws,
-            ]
-        ] = None,
+        llm_endpoint_params: Optional[AiLlmEndpointParams] = None,
         **kwargs
     ):
         """
@@ -60,8 +48,6 @@ class AiAgentBasicGenTool(AiAgentLongTextToolTextGen):
                 :type model: Optional[str], optional
                 :param num_tokens_for_completion: The number of tokens for completion., defaults to None
                 :type num_tokens_for_completion: Optional[int], optional
-                :param llm_endpoint_params: The parameters for the LLM endpoint specific to OpenAI / Google models., defaults to None
-                :type llm_endpoint_params: Optional[Union[AiLlmEndpointParamsOpenAi, AiLlmEndpointParamsGoogle, AiLlmEndpointParamsAws]], optional
         """
         super().__init__(
             embeddings=embeddings,
